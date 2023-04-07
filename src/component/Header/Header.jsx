bd0535da91d354545531a13d287f8987d2fc6d5f
My Secont Github Repo <hmsadikurrahman120@gmail.com>
2023-04-07 10:26:31 +0600

    ad to active link part

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Header.css'
+import ActiveLink from '../ActiveLink/ActiveLink';
 
 const Header = () => {
     return (
         <div className='md:flex justify-between bg-slate-100 py-4 px-2'>
             <h2 className='text-2xl font-extrabold '>React router</h2>
             <nav >
-                <Link to="/">Home</Link>
-                <Link to="about">About</Link>
-                <Link to="contact">Contact</Link>
-                <Link to="photos">Photos</Link>
+                <ActiveLink to="/">Home</ActiveLink>
+                <ActiveLink to="about">About</ActiveLink>
+                <ActiveLink to="contact">Contact</ActiveLink>
+                <ActiveLink to="photos">Photos</ActiveLink>
             </nav>
         </div>
     );
