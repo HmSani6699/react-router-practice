982c60c1d8ce279b270710ae5d4681eab12c063b
My Secont Github Repo <hmsadikurrahman120@gmail.com>
2023-04-07 11:13:37 +0600

    ad spinner part

--- src/component/Home/Home.jsx
+++ src/component/Home/Home.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import Header from '../Header/Header';
 import { Outlet, useNavigation } from 'react-router-dom';
+import Spinner from '../Spiner/Spinner';
 
 const Home = () => {
     const navigation = useNavigation()
     return (
         <div>
             <Header></Header>
-            <div className={navigation.state === "loading" ? "loading" : ""}></div>
+            <div className="">
+                {navigation.state === "loading" && <Spinner></Spinner>}
+            </div>
             <Outlet></Outlet>
         </div>
     );
