0e35c06bb1c9eda5f13bbb0091bfe6f24c0be7ea
My Secont Github Repo <hmsadikurrahman120@gmail.com>
2023-04-07 06:57:26 +0600

    ad to back button

--- src/component/Photo/Photo.jsx
+++ src/component/Photo/Photo.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 const Photo = ({ photo }) => {
     const { id, url } = photo;
+    const handelNavigate = useNavigate()
     return (
         <div className='bg-lime-700 rounded-md p-6'>
             <h1 className='text-center mb-4 text-4xl text-white font-extrabold'>ID: {id}</h1>
             <img src={url} alt="" />
             <Link to={`/photo/${id}`}><button className="w-full border-2 border-gray-700 rounded-lg mt-6 bg-amber-400 p-2">Photo details</button></Link>
+            <Link to={`/photo/${id}`}><button onClick={() => handelNavigate(`/photo/${id}`)} className="w-full border-2 border-gray-700 rounded-lg mt-6 bg-amber-400 p-2">Details</button></Link>
+
         </div>
     );
 };
--- src/component/PhotoDetails/PhotoDetails.jsx
+++ src/component/PhotoDetails/PhotoDetails.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
-import { useLoaderData } from 'react-router-dom';
+import { useLoaderData, useNavigate } from 'react-router-dom';
 
 const PhotoDetails = () => {
     const photo = useLoaderData();
     const { title, id, url } = photo;
+    const backNavigate = useNavigate()
     return (
         <div className='mt-12 mx-auto text-center'>
             <h5>ID: {id}</h5>
             <h1 className='text-4xl font-bold'>{title}</h1>
+            <button onClick={() => backNavigate(-1)} className="w-full border-2 border-gray-700 rounded-lg mt-6 bg-amber-400 p-2">Go back</button>
         </div>
     );
 };
